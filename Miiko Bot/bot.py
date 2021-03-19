9b2bf3293de4e9c6367852f07588ad5e978f883a
Jayson Chao <jaysonchao@gmail.com>
2021-03-18 17:51:49 -0700

    Bot Setup
    Change from client to bot setup, implement some basic commands.

--- Miiko Bot/bot.py
+++ Miiko Bot/bot.py
@@ -2,24 +2,47 @@
 # first test file for running discord bot
 
 import os
-
-import discord
+import re
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN') # Hidden
+CMD_PREFIX = '&'
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=CMD_PREFIX)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is connected to Discord.')
+    print(f'{bot.user} is connected to Discord.')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
-    response = "nano!"
-    await message.channel.send(response)
 
-client.run(TOKEN)
+    if re.search('nano', message.content.lower()) and message.content[0] != CMD_PREFIX: 
+        await message.channel.send('nano!')
+
+    await bot.process_commands(message) # process commands, need bc on_message override
+
+@bot.command(name='ping', help='test command & response')
+async def shh_nano(ctx):
+    response = 'pong, nano!'
+    await ctx.send(response)
+
+@bot.command(name='admin', help='test admin-only command')
+@commands.has_permissions(administrator=True)
+async def admin_ping(ctx):
+    response = 'admin, nano!'
+    await ctx.send(response)
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+bot.run(TOKEN)
